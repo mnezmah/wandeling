06f69d297c93469a3456578cff7ac5afc9d0753e
mario <marionezmah@kateinnovations.com>
2021-05-13 13:00:59 +0200

    Install dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
+import Home from "./components/Home/Home";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                Learn React
-            </header>
+            <Home />
         </div>
     );
 }
